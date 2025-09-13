75871303e1330983d20524b77e070271508fa919
Navith <navithnavithnavith+git@gmail.com>
2025-09-12 22:07:56 -0400

    feat: signature verification

--- via-axum/src/lib.rs
+++ via-axum/src/lib.rs
@@ -1,5 +1,60 @@
 use axum::Router;
+use ed25519_compact::{PublicKey, Signature};
+use snafu::Snafu;
 
 pub fn create_router() -> Router<()> {
     todo!()
+}
+
+#[derive(Debug, Snafu)]
+pub enum VerificationError {
+    #[snafu(display("signature was not provided"))]
+    MissingSignature,
+    #[snafu(display("timestamp was not provided"))]
+    MissingTimestamp,
+    #[snafu(display("body was not provided"))]
+    MissingBody,
+    #[snafu(display("public key was not provided"))]
+    MissingPublicKey,
+
+    #[snafu(display("the given signature is not represented in hex"))]
+    SignatureInvalidHex,
+    #[snafu(display("the given signature does not represent a valid ED25519 compact signature"))]
+    SignatureInvalidKey,
+
+    #[snafu(display("the given public key is not represented in hex"))]
+    PublicKeyInvalidHex,
+    #[snafu(display("the given public key does not represent a valid ED25519 compact public key"))]
+    PublicKeyInvalidKey,
+
+    #[snafu(display(
+        "all the needed information was provided, but this message was not signed with the private key corresponding to this public key, so something suspicious may be going on"
+    ))]
+    DoesNotVerify,
+}
+
+pub fn verify(
+    signature: Option<&str>,
+    timestamp: Option<&str>,
+    body: Option<&str>,
+    public_key: Option<&str>,
+) -> Result<(), VerificationError> {
+    use VerificationError::*;
+
+    let signature = signature.ok_or(MissingSignature)?;
+    let timestamp = timestamp.ok_or(MissingTimestamp)?;
+    let body = body.ok_or(MissingBody)?;
+    let public_key = public_key.ok_or(MissingPublicKey)?;
+
+    let message = [timestamp.as_bytes(), body.as_bytes()].concat();
+
+    let signature = hex::decode(signature).map_err(|_e| SignatureInvalidHex)?;
+    let signature = Signature::from_slice(&signature).map_err(|_e| SignatureInvalidKey)?;
+
+    let public_key = hex::decode(public_key).map_err(|_e| PublicKeyInvalidHex)?;
+    let public_key = PublicKey::from_slice(&public_key).map_err(|_e| PublicKeyInvalidKey)?;
+
+    public_key
+        .verify(message, &signature)
+        .map_err(|_e| DoesNotVerify)
 }
